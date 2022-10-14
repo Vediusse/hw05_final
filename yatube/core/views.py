f867889728994135703b66083b7aa1470572bb5b
Valerii Rublev <vedius.rublev@gmail.com>
2022-10-14 19:37:24 +0300

    final

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html', {'path': request.path}, status=HTTPStatus.NOT_FOUND.value)
+    return render(request,
+                  'core/404.html',
+                  {'path': request.path},
+                  status=HTTPStatus.NOT_FOUND.value
+                  )
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html', status=HTTPStatus.FORBIDDEN.value)
+    return render(request,
+                  'core/403csrf.html',
+                  status=HTTPStatus.FORBIDDEN.value
+                  )
 
 
 def server_error(request):
-    return render(request, 'core/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR.value)
+    return render(request,
+                  'core/500.html',
+                  status=HTTPStatus.INTERNAL_SERVER_ERROR.value
+                  )
